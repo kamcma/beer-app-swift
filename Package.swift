b157236db185254a37110a64545d431424c4962a
Kyle McMahon <kyle.mcmahon@mac.com>
2016-12-05 20:26:06 -0500

    Configure in-memory datastore for development

--- Package.swift
+++ Package.swift
@@ -4,7 +4,8 @@ let package = Package(
     name: "BeerApp",
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1),
-        .Package(url: "https://github.com/vapor/postgresql-provider.git", majorVersion: 1)
+        .Package(url: "https://github.com/vapor/postgresql-provider.git", majorVersion: 1),
+        .Package(url: "https://github.com/sfaxon/vapor-memory-provider.git", majorVersion: 1)
     ],
     exclude: [
         "Config",
--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -1,26 +1,24 @@
 import Vapor
 import VaporPostgreSQL
+import VaporMemory
 
 let drop = Droplet(
     preparations: [Brewery.self, Beer.self],
     providers: [VaporPostgreSQL.Provider.self]
 )
 
-drop.get("version") {request in
-    if let db = drop.database?.driver as? PostgreSQLDriver {
-        let version = try db.raw("select version()")
-        return try JSON(node: version)
-    } else {
-        return "No db connection"
-    }
+if drop.environment == .production {
+    try drop.addProvider(VaporPostgreSQL.Provider.self)
+} else {
+    try drop.addProvider(VaporMemory.Provider.self)
 }
 
-drop.get("breweries") { request in
-    return "Not yet implemented"
+
+drop.get("brewery") { request in
+    var brewery = Brewery(name: "Great Lakes Brewing Company")
+    try brewery.save()
+    return try JSON(node: Brewery.all().makeNode())
 }
 
-drop.get("beers") { request in
-    return "Not yet implemented"
-}
 
 drop.run()
