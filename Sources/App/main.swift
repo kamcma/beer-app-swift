37648ab0e08b08db8c97cb64023f5f5f2132d5ed
Kyle McMahon <kyle.mcmahon@mac.com>
2017-02-21 23:33:59 -0500

    Add controllers to droplet

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -4,15 +4,9 @@ import VaporPostgreSQL
 let drop = Droplet()
 
 try drop.addProvider(VaporPostgreSQL.Provider.self)
+//drop.preparations = [Brewery.self]
 
-drop.preparations = [Brewery.self]
-
-drop.get("breweries") { _ in
-    return try JSON(node: Brewery.query().sort("name", .ascending).all().makeNode())
-}
-
-drop.get("beers") { _ in
-    return try JSON(node: Beer.query().sort("name", .ascending).all().makeNode())
-}
+AdminController().addRoutes(drop: drop)
+UserController().addRoutes(drop: drop)
 
 drop.run()
