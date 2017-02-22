84b2e053a2ca846dc944d0f7590b1437db13bf36
Kyle McMahon <kyle.mcmahon@mac.com>
2017-02-21 22:30:20 -0500

    Stub user controller

---
+++ Sources/App/Controllers/UserController.swift
@@ -0,0 +1,14 @@
+import Vapor
+import HTTP
+import VaporPostgreSQL
+
+final class UserController {
+
+    func addRoutes(drop: Droplet) {
+        /*drop.get("register", handler: nil)
+        drop.post("register", handler: nil)
+        drop.get("login", handler: nil)
+        drop.post("login", handler: nil)*/
+    }
+
+}
