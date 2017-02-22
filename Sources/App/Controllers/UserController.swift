607d618b30ae3b3eae1c80a5cee244bac9f29fb4
Kyle McMahon <kyle.mcmahon@mac.com>
2017-02-21 22:37:41 -0500

    Add a single register get route

--- Sources/App/Controllers/UserController.swift
+++ Sources/App/Controllers/UserController.swift
@@ -5,10 +5,18 @@ import VaporPostgreSQL
 final class UserController {
 
     func addRoutes(drop: Droplet) {
-        /*drop.get("register", handler: nil)
-        drop.post("register", handler: nil)
+        drop.get("register", handler: registerView)
+        /*drop.post("register", handler: nil)
         drop.get("login", handler: nil)
         drop.post("login", handler: nil)*/
     }
+
+    func registerView(request: Request) throws -> ResponseRepresentable {
+        return try drop.view.make("register")
+    }
+
+    /*func register(request: Request) throws -> ResponseRepresentable {
+        throw 
+    }*/
 
 }
