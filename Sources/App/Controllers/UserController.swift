e9566e1b03aacc2d31a582abe49e2b0883f574ce
Kyle McMahon <kyle.mcmahon@mac.com>
2017-02-22 23:37:12 -0500

    Implement basic register and login pages

--- Sources/App/Controllers/UserController.swift
+++ Sources/App/Controllers/UserController.swift
@@ -6,13 +6,17 @@ final class UserController {
 
     func addRoutes(drop: Droplet) {
         drop.get("register", handler: registerView)
-        /*drop.post("register", handler: nil)
-        drop.get("login", handler: nil)
-        drop.post("login", handler: nil)*/
+        //drop.post("register", handler: nil)
+        drop.get("login", handler: loginView)
+        //drop.post("login", handler: nil)
     }
 
     func registerView(request: Request) throws -> ResponseRepresentable {
         return try drop.view.make("register")
+    }
+
+    func loginView(request: Request) throws -> ResponseRepresentable {
+        return try drop.view.make("login")
     }
 
     /*func register(request: Request) throws -> ResponseRepresentable {
