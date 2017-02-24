d9523c49f880e09aca26dca75a708f6981411e18
Kyle McMahon <kyle.mcmahon@mac.com>
2017-02-23 22:29:05 -0500

    Reimplement User class based on Stormpath tutorial

--- Sources/App/Models/User.swift
+++ Sources/App/Models/User.swift
@@ -2,90 +2,49 @@ import Vapor
 import Fluent
 import Turnstile
 import TurnstileCrypto
+import Auth
 
-final class User: Model {
+final class User: Auth.User {
     var exists: Bool = false
     var id: Node?
-    var firstName: Valid<NameValidator>
-    var lastName: Valid<NameValidator>
-    var email: Valid<Email>
+    var username: String
     var password: String
 
-    init (firstName: String, lastName: String, email: String, rawPassword: String) throws {
-        self.firstName = try firstName.validated()
-        self.lastName = try lastName.validated()
-        self.email = try email.validated()
-        let validatedPassword: Valid<PasswordValidator> = try rawPassword.validated()
-        self.password = BCrypt.hash(password: validatedPassword.value)
+    init (credentials: UsernamePassword) {
+        self.username = credentials.username
+        self.password = BCrypt.hash(password: credentials.password)
     }
 
     init (node: Node, in context: Context) throws {
         id = node["id"]
-        let firstNameString = try node.extract("first_name") as String
-        firstName = try firstNameString.validated()
-        let lastNameString = try node.extract("last_name") as String
-        lastName = try lastNameString.validated()
-        let emailString = try node.extract("email") as String
-        email = try emailString.validated()
-        let passwordString = try node.extract("password") as String
-        password = passwordString
+        username = try node.extract("username") as String
+        password = try node.extract("password") as String
+
     }
 
     func makeNode(context: Context) throws -> Node {
         return try Node(node: [
             "id": id,
-            "first_name": firstName.value,
-            "last_name": lastName.value,
-            "email": email.value,
+            "username": username,
             "password": password
         ])
     }
 
-    static func prepare(_ database: Database) throws {
-        try database.create("users") {users in
-            users.id()
-            users.string("first_name")
-            users.string("last_name")
-            users.string("email")
-            users.string("password")
-
-        }
-    }
-
-    static func revert(_ database: Database) throws {
-        try database.delete("users")
-    }
-
-    static func register(firstName: String, lastName: String, email: String, rawPassword: String) throws -> User {
-        var newUser = try User(firstName: firstName, lastName: lastName, email: email, rawPassword: rawPassword)
-        if try User.query().filter("email", newUser.email.value).first() == nil {
-            try newUser.save()
-            return newUser
-        } else {
-            throw AccountTakenError()
-        }
-    }
-}
-
-import Auth
-
-extension User: Auth.User, Authenticator {
     static func authenticate(credentials: Credentials) throws -> Auth.User {
         var user: User?
 
         switch credentials {
-            case let credentials as UsernamePassword:
-                let fetchedUser = try User.query()
-                    .filter("email", credentials.username)
-                    .first()
-                if let password = fetchedUser?.password, password != "",
-                    (try? BCrypt.verify(password: credentials.password, matchesHash: password)) == true {
-                        user = fetchedUser
-                }
-            case let credentials as Identifier:
-                user = try User.find(credentials.id)
-            default:
-                throw UnsupportedCredentialsError()
+        case let credentials as UsernamePassword:
+            let fetchedUser = try User.query()
+                .filter("username", credentials.username).first()
+            if let password = fetchedUser?.password, password != "",
+                (try? BCrypt.verify(password: credentials.password, matchesHash: password)) == true {
+                user = fetchedUser
+            }
+        case let credentials as Identifier:
+            user = try User.find(credentials.id)
+        default:
+            throw UnsupportedCredentialsError()
         }
 
         if let user = user {
@@ -96,6 +55,32 @@ extension User: Auth.User, Authenticator {
     }
 
     static func register(credentials: Credentials) throws -> Auth.User {
-        throw Abort.badRequest
+        var newUser: User
+
+        switch credentials {
+        case let credentials as UsernamePassword:
+            newUser = User(credentials: credentials)
+        default:
+            throw UnsupportedCredentialsError()
+        }
+
+        if try User.query().filter("username", newUser.username).first() == nil {
+            try newUser.save()
+            return newUser
+        } else {
+            throw AccountTakenError()
+        }
+    }
+
+    static func prepare(_ database: Database) throws {
+        try database.create("users") {users in
+            users.id()
+            users.string("username")
+            users.string("password")
+        }
+    }
+
+    static func revert(_ database: Database) throws {
+        try database.delete("users")
     }
 }
