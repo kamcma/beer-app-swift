431ed19254aa764b381eee9594bb6a9b0c3b7f97
Kyle McMahon <kyle.mcmahon@mac.com>
2017-02-23 23:09:52 -0500

    Update preparations, implement root route

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -1,12 +1,18 @@
 import Vapor
 import VaporPostgreSQL
+import Auth
 
 let drop = Droplet()
 
 try drop.addProvider(VaporPostgreSQL.Provider.self)
-//drop.preparations = [Brewery.self]
+drop.preparations = [Brewery.self, Beer.self, User.self]
+
+drop.middleware.append(AuthMiddleware(user: User.self))
 
 AdminController().addRoutes(drop: drop)
 UserController().addRoutes(drop: drop)
+drop.get("/") { _ in
+    return try drop.view.make("index")
+}
 
 drop.run()
