01a35443546b66727bac6252421c5f10b5ceba96
Kyle McMahon <kyle.mcmahon@mac.com>
2017-03-04 23:11:35 -0500

    Reimplement root route, Auth middleware, remove unnecessary imports

--- Sources/AppLogic/setup.swift
+++ Sources/AppLogic/setup.swift
@@ -1,12 +1,17 @@
 import Vapor
 import VaporPostgreSQL
-import Fluent
-import HTTP
+import Auth
 
 public func setup(_ drop: Droplet) throws {
     try drop.addProvider(VaporPostgreSQL.Provider.self)
     drop.preparations = [Brewery.self, Beer.self, User.self]
 
+    drop.middleware.append(AuthMiddleware(user: User.self))
+
     AdminController(drop: drop).addRoutes()
     UserController(drop: drop).addRoutes()
+
+    drop.get("/") { _ in
+        return try drop.view.make("index")
+    }
 }
