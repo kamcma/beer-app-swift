834f7941fab408a37249eacf4371862cf62edcd2
Kyle McMahon <kyle.mcmahon@mac.com>
2017-03-07 23:17:46 -0500

    Back country enumeration with integers

--- Sources/AppLogic/Models/Enumerations.swift
+++ Sources/AppLogic/Models/Enumerations.swift
@@ -1,3 +1,12 @@
-enum Country {
-    case Brazil, China, Germany, Japan, Mexico, Poland, Russia, Spain, UnitedKingdom, UnitedStates
+enum Country: Int {
+    case brazil = 076
+    case china = 156
+    case germany = 276
+    case japan = 392
+    case mexico = 484
+    case poland = 616
+    case russia = 643
+    case spain = 724
+    case unitedKingdom = 826
+    case unitedStates = 840
 }
