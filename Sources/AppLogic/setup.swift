6f6aa73addc224f2c1da999d01d6a0f781c9717c
Kyle McMahon <kyle.mcmahon@mac.com>
2017-03-08 20:39:47 -0500

    Reconfigure adding preparations to Droplet

--- Sources/AppLogic/setup.swift
+++ Sources/AppLogic/setup.swift
@@ -1,11 +1,21 @@
 import Vapor
 import VaporPostgreSQL
+import Fluent
 import Auth
 
 public func setup(_ drop: Droplet) throws {
     try drop.addProvider(VaporPostgreSQL.Provider.self)
-    drop.preparations = [M20170307185700InitialMigration.self]
-    //drop.commands.append(Seed(console: drop.console))
+
+    let models: [Preparation.Type] = [
+        User.self, Brewery.self, Beer.self, BeerRating.self
+    ]
+    let migrations: [Preparation.Type] = [
+        M20170307185700InitialMigration.self
+    ]
+
+    for item in models + migrations {
+        drop.preparations.append(item)
+    }
 
     drop.middleware.append(AuthMiddleware(user: User.self))
 
