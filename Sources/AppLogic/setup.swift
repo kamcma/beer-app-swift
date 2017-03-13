08874294ff1e11c149bac6407b32c4c766f260b0
Kyle McMahon <kyle.mcmahon@mac.com>
2017-03-13 00:03:38 -0400

    Implement auth middleware, add ssl middleware

--- Sources/AppLogic/setup.swift
+++ Sources/AppLogic/setup.swift
@@ -2,6 +2,9 @@ import Vapor
 import VaporPostgreSQL
 import Fluent
 import Auth
+import Foundation
+import Cookies
+import XFPMiddleware
 
 public func setup(_ drop: Droplet) throws {
     try drop.addProvider(VaporPostgreSQL.Provider.self)
@@ -17,7 +20,17 @@ public func setup(_ drop: Droplet) throws {
         drop.preparations.append(item)
     }
 
-    drop.middleware.append(AuthMiddleware(user: User.self))
+    drop.commands.append(Seed(drop: drop))
+
+    drop.middleware.append(XFPMiddleware(enabled: drop.environment == .production))
+    drop.middleware.append(AuthMiddleware(user: User.self) { value in
+        return Cookie(
+            name: "beer-app",
+            value: value,
+            expires: Date().addingTimeInterval(60 * 60 * 5),
+            secure: drop.environment == .production
+        )
+    })
 
     AdminController(drop: drop).addRoutes()
     UserController(drop: drop).addRoutes()
