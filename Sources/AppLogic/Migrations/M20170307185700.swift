299be88195bb7d65ac0349aec41acd20f9dc455a
Kyle McMahon <kyle.mcmahon@mac.com>
2017-03-25 20:18:55 -0400

    Correct optional parameter settings

--- Sources/AppLogic/Migrations/M20170307185700.swift
+++ Sources/AppLogic/Migrations/M20170307185700.swift
@@ -5,17 +5,17 @@ struct M20170307185700InitialMigration: Preparation {
     static func prepare(_ database: Database) throws {
         try database.create(Brewery.entity) { breweries in
             breweries.id()
-            breweries.string("name", length: 255, optional: false)
-            breweries.string("slug", length: 255, optional: false, unique: true)
-            breweries.int("country")
+            breweries.string("name", length: 255)
+            breweries.string("slug", length: 255, unique: true)
+            breweries.int("country", optional: true)
         }
         try database.create(Beer.entity) { beers in
             beers.id()
-            beers.string("name", length: 255, optional: false)
-            beers.string("slug", length: 255, optional: false)
-            beers.parent(Brewery.self, optional: false)
-            beers.double("abv")
-            beers.int("ibu")
+            beers.string("name", length: 255)
+            beers.string("slug", length: 255)
+            beers.parent(Brewery.self)
+            beers.double("abv", optional: true)
+            beers.int("ibu", optional: true)
         }
         try database.driver.raw("ALTER TABLE \"public\".\"beers\" "
             + "ADD FOREIGN KEY (\"brewery_id\") "
@@ -23,17 +23,17 @@ struct M20170307185700InitialMigration: Preparation {
             + "ON DELETE CASCADE ON UPDATE CASCADE;")
         try database.create(User.entity) {users in
             users.id()
-            users.string("email", length: 255, optional: false, unique: true)
-            users.string("password", optional: false)
+            users.string("email", length: 255, unique: true)
+            users.string("password")
             users.string("first_name", length: 255)
             users.string("last_name", length: 255)
             users.bool("admin")
         }
         try database.create(BeerRating.entity) { beerRatings in
             beerRatings.id()
-            beerRatings.parent(User.self, optional: false)
-            beerRatings.parent(Beer.self, optional: false)
-            beerRatings.bool("thumb_up", optional: false)
+            beerRatings.parent(User.self)
+            beerRatings.parent(Beer.self)
+            beerRatings.bool("thumb_up")
         }
         try database.driver.raw("ALTER TABLE \"public\".\"beerratings\" "
             + "ADD FOREIGN KEY (\"user_id\") "
